2e8cd8c3ca13d6bf224b50c053c7574cf54992d7
gemo <kim.gemo@gmail.com>
2024-09-22 15:58:01 +0200

    updated bruteforce to open csv file

--- bruteforce.py
+++ bruteforce.py
@@ -1,36 +1,47 @@
+from itertools import combinations
+import csv
+
 class Action:
     def __init__(self, name, price, profit):
         self.name = name
         self.price = price
         self.profit = profit
 
+
 class List_of_Actions:
     def __init__(self, list_of_actions: list[Action]):
         self.list_of_actions = list_of_actions
+    
+    # def manual_possibilities_to_target(self, numbers, target, results, partial=[]):
+    #     s = 0
+    #     for each in partial:
+    #         if isinstance(each, Action):
+    #             s += each.price
+    #     if s <= target:
+    #         results.append(partial)
+    #     if s >= target:
+    #         return
+    #     for i in range(len(numbers)):
+    #         n = numbers[i]
+    #         remaining = numbers[i + 1:]
+    #         self.manual_possibilities_to_target(remaining, target, results, partial + [n])
+    
+    def find_all_sets_to_target(self, target):
+        all_possibilities = []
+        for count in range(1, len(self.list_of_actions)):
+            for actions_combination in combinations(self.list_of_actions, count):
+                if sum(action.price for action in actions_combination) <= target:
+                    all_possibilities.append(actions_combination)
+        return all_possibilities
 
-    def subset_sum(self, numbers, target, results, partial=[]):
-        s = 0
-        for each in partial:
-            if isinstance(each, Action):
-                s += each.price
-
-        if s == target:
-            results.append(partial)
-        if s >= target:
-            return
-
-        for i in range(len(numbers)):
-            n = numbers[i]
-            remaining = numbers[i + 1:]
-            self.subset_sum(remaining, target, results, partial + [n])
-
-    def profit_eval(self):
-        results = []
+    def find_maximum_profit_set(self):
         target = 500
-        self.subset_sum(self.list_of_actions, target, results)
+        # results = []
+        # self.all_possibilities_equal_to_target(self.list_of_actions, target, results)
+        results = self.find_all_sets_to_target(target)
         
         maximum_profit = 0
-        maximum_result = None
+        maximum_result = []
         for result in results:
             total_profit = 0
             for action in result:
@@ -45,33 +56,18 @@ class List_of_Actions:
             
 
 if __name__ == "__main__":
-    my_list = List_of_Actions(
-        [
-            Action("Action-1", 20, 5),
-            Action("Action-2", 30, 10),
-            Action("Action-3", 50, 15),
-            Action("Action-4", 70, 20),
-            Action("Action-5", 60, 17),
-            Action("Action-6", 80, 25),
-            Action("Action-7", 22, 7),
-            Action("Action-8", 26, 11),
-            Action("Action-9", 48, 13),
-            Action("Action-10", 34, 27),
-            Action("Action-11", 42, 17),
-            Action("Action-12", 110, 9),
-            Action("Action-13", 38, 23),
-            Action("Action-14", 14, 1),
-            Action("Action-15", 18, 3),
-            Action("Action-16", 8, 8),
-            Action("Action-17", 4, 12),
-            Action("Action-18", 10, 14),
-            Action("Action-19", 24, 21),
-            Action("Action-20", 114, 18),
-        ]
-    )
+    with open('datasets/liste_actions.csv') as file:
+        reader = csv.reader(file)
+        next(reader, None) #skip header
+        list_of_actions = []
+        for row in reader:
+            list_of_actions.append(Action(row[0], int(row[1]), int(row[2].strip("%"))))
+            
+        my_list = List_of_Actions(list_of_actions=list_of_actions)
     
-    maximum_result, maximum_profit = my_list.profit_eval()
+    maximum_result, maximum_profit = my_list.find_maximum_profit_set()
     
     for action in maximum_result:
         print(action.__dict__)
-    print(f"Maximum profit is {maximum_profit}")
+    print(f"Maximum profit is {round(maximum_profit, 2)}")
+    print(f"Invested amount is {sum(action.price for action in maximum_result)}")
