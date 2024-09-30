841ed7790c82cc39c96eee14ef3b4492527ef53b
gemo <kim.gemo@gmail.com>
2024-09-30 21:44:45 +0200

    finishing touch bruteforce algo

--- bruteforce.py
+++ bruteforce.py
@@ -1,5 +1,8 @@
+import argparse
 from itertools import combinations
 import csv
+import sys
+
 
 class Action:
     def __init__(self, name, price, profit):
@@ -11,7 +14,7 @@ class Action:
 class List_of_Actions:
     def __init__(self, list_of_actions: list[Action]):
         self.list_of_actions = list_of_actions
-    
+
     # def manual_possibilities_to_target(self, numbers, target, results, partial=[]):
     #     s = 0
     #     for each in partial:
@@ -25,7 +28,7 @@ class List_of_Actions:
     #         n = numbers[i]
     #         remaining = numbers[i + 1:]
     #         self.manual_possibilities_to_target(remaining, target, results, partial + [n])
-    
+
     def find_all_sets_to_target(self, target):
         all_possibilities = []
         for count in range(1, len(self.list_of_actions)):
@@ -39,35 +42,46 @@ class List_of_Actions:
         # results = []
         # self.all_possibilities_equal_to_target(self.list_of_actions, target, results)
         results = self.find_all_sets_to_target(target)
-        
+
         maximum_profit = 0
         maximum_result = []
         for result in results:
             total_profit = 0
             for action in result:
-                total_profit += action.price + (action.price * action.profit / 100)
+                total_profit += action.price * action.profit / 100
             if total_profit > maximum_profit:
                 maximum_profit = total_profit
                 maximum_result = result
             else:
                 continue
-        
+
         return maximum_result, maximum_profit
-            
+
 
 if __name__ == "__main__":
-    with open('datasets/liste_actions.csv') as file:
-        reader = csv.reader(file)
-        next(reader, None) #skip header
-        list_of_actions = []
-        for row in reader:
-            list_of_actions.append(Action(row[0], int(row[1]), int(row[2].strip("%"))))
-            
-        my_list = List_of_Actions(list_of_actions=list_of_actions)
-    
-    maximum_result, maximum_profit = my_list.find_maximum_profit_set()
-    
-    for action in maximum_result:
-        print(action.__dict__)
-    print(f"Maximum profit is {round(maximum_profit, 2)}")
-    print(f"Invested amount is {sum(action.price for action in maximum_result)}")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("csv", help="csv file to analyze")
+    args = parser.parse_args()
+
+    if args.csv:
+        with open(args.csv, "r") as file:
+            reader = csv.reader(file)
+            next(reader, None)  # skip header
+            list_of_actions = []
+            for row in reader:
+                list_of_actions.append(
+                    Action(row[0], int(row[1]), int(row[2].strip("%")))
+                )
+
+            my_list = List_of_Actions(list_of_actions=list_of_actions)
+            file.close()
+
+        maximum_result, maximum_profit = my_list.find_maximum_profit_set()
+
+        print("The brute force algorithm bought:")
+
+        for action in maximum_result:
+            print(action.name)
+
+        print(f"Total cost: {sum(action.price for action in maximum_result)}")
+        print(f"Total return: {round(maximum_profit, 2)}")
